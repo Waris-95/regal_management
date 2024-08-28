2ed452e68b7402fcc9185c766b73fb3ff706a8a1
Abdul Waris <maiyaleeg223@gmail.com>
2024-08-28 18:44:25 -0400

    redux toolkit setup

--- client/src/app/redux.tsx
+++ client/src/app/redux.tsx
@@ -7,7 +7,7 @@ import {
   Provider,
 } from "react-redux";
 import globalReducer from "./state/index";
-import { api } from "@/state/api";
+import { api } from "./state/api";
 import { setupListeners } from "@reduxjs/toolkit/query";
 
 import {
