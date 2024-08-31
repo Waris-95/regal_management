81bd5b805c91ecc8d657806b9c4dbea48dd21651
Abdul Waris <maiyaleeg223@gmail.com>
2024-08-31 15:27:31 -0400

    changes to dashboard

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,14 +1,16 @@
 "use client"
 
+import PopularProducts from "./PopularProducts"
+
 const Dashboard = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows">
-      <div className="row-span-3 xl:row-span-6 bg-gray-500"/>
+      <PopularProducts/>
       <div className="row-span-3 xl:row-span-6 bg-gray-500"/>
       <div className="row-span-3 bg-gray-500"/>
       <div className="md:row-span-1 xl:row-span-2 bg-gray-500" />
       <div className="md:row-span-1 xl:row-span-2 bg-gray-500" />
-      <div className="md:row-span-1 xl:row-span-2 bg-gray-500"/>
+      <div className="md:row-span-1 xl:row-span-2 bg-gray-500" />
     </div>
   )
 }
