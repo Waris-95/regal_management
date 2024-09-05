9a486991344b083250f50ef11d25d80a7de3d8a0
Abdul Waris <maiyaleeg223@gmail.com>
2024-09-05 11:51:10 -0400

    completed expense summary

--- client/src/app/dashboard/cardExpenseSummary.tsx
+++ client/src/app/dashboard/cardExpenseSummary.tsx
@@ -1,27 +1,132 @@
-import { useGetDashboardMetricsQuery } from "../state/api";
+import {
+  ExpenseByCategorySummary,
+  useGetDashboardMetricsQuery,
+} from "../state/api";
+import { TrendingUp } from "lucide-react";
+import { Cell, Pie, PieChart, ResponsiveContainer } from "recharts";
 
-const colors = ['#00C49F', '#0088FE', '#FFBB28'];
+type ExpenseSums = {
+  [category: string]: number;
+};
+
+const colors = ["#00C49F", "#0088FE", "#FFBB28"];
 
 const CardExpenseSummary = () => {
-  const { data: dashboardmetrics, isLoading } = useGetDashboardMetricsQuery();
+  const { data: dashboardMetrics, isLoading } = useGetDashboardMetricsQuery();
+
+  const expenseSummary = dashboardMetrics?.expenseSummary[0];
+
+  const expenseByCategorySummary =
+    dashboardMetrics?.expenseByCategorySummary || [];
+
+  const expenseSums = expenseByCategorySummary.reduce(
+    (acc: ExpenseSums, item: ExpenseByCategorySummary) => {
+      const category = item.category + " Expenses";
+      const amount = parseInt(item.amount, 10);
+      if (!acc[category]) acc[category] = 0;
+      acc[category] += amount;
+      return acc;
+    },
+    {}
+  );
+
+  const expenseCategories = Object.entries(expenseSums).map(
+    ([name, value]) => ({
+      name,
+      value,
+    })
+  );
+
+  const totalExpenses = expenseCategories.reduce(
+    (acc, category: { value: number }) => acc + category.value,
+    0
+  );
+  const formattedTotalExpenses = totalExpenses.toFixed(2);
 
   return (
-    <div className="row-span-3 bg-white shadow-md rounded-2xl flex flex-col justify-between">
+    <div className="flex flex-col justify-between row-span-2 xl:row-span-3 col-span-1 md:col-span-2 xl:col-span-1 bg-gradient-to-br from-white via-black-50 to-gray-100 shadow-lg rounded-2xl transition-all duration-500 ease-in-out transform hover:scale-105 hover:shadow-2xl">
       {isLoading ? (
-        <div className="m-5 animate-pulse">Loading...</div>
+        <div className="m-5">Loading...</div>
       ) : (
         <>
-         {/* HEADER */}
-         <div>
-            <h2 className="text-lg font-semibold mb-2 px-7 pt-5 transition-colors duration-500 ease-in-out hover:text-blue-600">
+          {/* HEADER */}
+          <div>
+            <h2 className="text-lg font-semibold mb-2 px-7 pt-5">
               Expense Summary
             </h2>
             <hr />
           </div>
+          {/* BODY */}
+          <div className="xl:flex justify-between pr-7">
+            {/* CHART */}
+            <div className="relative basis-3/5">
+              <ResponsiveContainer width="100%" height={140}>
+                <PieChart>
+                  <Pie
+                    data={expenseCategories}
+                    innerRadius={50}
+                    outerRadius={60}
+                    fill="#8884d8"
+                    dataKey="value"
+                    nameKey="name"
+                    cx="50%"
+                    cy="50%"
+                  >
+                    {expenseCategories.map((entry, index) => (
+                      <Cell
+                        key={`cell-${index}`}
+                        fill={colors[index % colors.length]}
+                      />
+                    ))}
+                  </Pie>
+                </PieChart>
+              </ResponsiveContainer>
+              <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center basis-2/5">
+                <span className="font-bold text-xl">
+                  ${formattedTotalExpenses}
+                </span>
+              </div>
+            </div>
+            {/* LABELS */}
+            <ul className="flex flex-col justify-around items-center xl:items-start py-5 gap-3">
+              {expenseCategories.map((entry, index) => (
+                <li
+                  key={`legend-${index}`}
+                  className="flex items-center text-xs"
+                >
+                  <span
+                    className="mr-2 w-3 h-3 rounded-full"
+                    style={{ backgroundColor: colors[index % colors.length] }}
+                  ></span>
+                  {entry.name}
+                </li>
+              ))}
+            </ul>
+          </div>
+          {/* FOOTER */}
+          <div>
+            <hr />
+            {expenseSummary && (
+              <div className="mt-3 flex justify-between items-center px-7 mb-4">
+                <div className="pt-2">
+                  <p className="text-sm">
+                    Average:{" "}
+                    <span className="font-semibold">
+                      ${expenseSummary.totalExpenses.toFixed(2)}
+                    </span>
+                  </p>
+                </div>
+                <span className="flex items-center mt-2">
+                  <TrendingUp className="mr-2 text-green-500" />
+                  30%
+                </span>
+              </div>
+            )}
+          </div>
         </>
       )}
-      </div>
-  )
-}
+    </div>
+  );
+};
 
-export default CardExpenseSummary
+export default CardExpenseSummary;
